317e4527aaf7be61fca4a2bedda64980d292ed38
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2015-08-28 14:16:57 +0200

    updated server.js

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
 require('harp').server(__dirname, { port: process.env.PORT || 5000 })
+console.log('HARP SERVER STARTED')
+console.log('SERVER IS RUNNING ON PORT ',process.env.PORT || 5000)
+console.log('SERVER ENV IS ', process.env.NODE_ENV || 'development')
